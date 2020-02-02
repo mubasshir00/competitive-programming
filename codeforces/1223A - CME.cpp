ce508bbf2a0b54f9ecaa2ace4f39918c0480be93
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-02-02 10:29:32 +0600

    Create 1223A - CME.cpp

---
+++ codeforces/1223A - CME.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    long long n ;
+    cin>>n;
+    while(n--)
+    {
+        long long tc;
+        cin>>tc;
+        if(tc==2)cout<<"2"<<endl;
+     
+       
+        else if(tc%2==1)
+        {
+            cout<<"1"<<endl;
+        }
+        else cout<<"0"<<endl;
+
+    }
+    return 0;
+}
