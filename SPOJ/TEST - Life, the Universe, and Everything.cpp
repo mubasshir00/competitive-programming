1d6cc77225df685929eaf75ffea766da71ffff57
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-03-09 01:28:40 +0600

    Create TEST - Life, the Universe, and Everything.cpp

---
+++ SPOJ/TEST - Life, the Universe, and Everything.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    string s ;
+    while(cin>>s)
+    {
+        if(s!="42")
+        {
+            cout<<s<<endl;
+        }
+        else
+        {
+            break ;
+        }
+    }
+    return 0 ;
+}
