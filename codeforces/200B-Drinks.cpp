850a495f8b20d51fac3a58d1a30ec592de582bdc
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-03-25 12:11:25 +0600

    Update 200B-Drinks.cpp

--- codeforces/200B-Drinks.cpp
+++ codeforces/200B-Drinks.cpp
@@ -2,20 +2,18 @@
 using namespace std ;
 int main()
 {
-    int  a[1008];
-    int n ;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    double sum =0 ;
-    for(int i=0;i<n;i++)
-    {
-        sum=sum+a[i];
-    }
-    double av  = 0;
-    av= sum/n;
-    printf("%.12lf\n",av);
+   float n ,sum=0 ;
+   cin>>n;
+   int arr[1000];
+   for(int i=0;i<n;i++)
+   {
+       cin>>arr[i];
+   }
+   for(int i=0;i<n;i++)
+   {
+       sum=sum+arr[i];
+   }
+   printf("%.12f",sum/n);
+
     return 0 ;
 }
