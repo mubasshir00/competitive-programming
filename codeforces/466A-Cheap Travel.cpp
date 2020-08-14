3636646970835b77a08cfbc8f84ef95eae9f7e19
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-08-15 00:43:45 +0600

    Update 466A-Cheap Travel.cpp

--- codeforces/466A-Cheap Travel.cpp
+++ codeforces/466A-Cheap Travel.cpp
@@ -2,9 +2,20 @@
 using namespace std ;
 int main()
 {
-    int n,m,a,b;
+    int n ,m,a,b;
     cin>>n>>m>>a>>b;
-    cout<<min((n*a), (((n/m)*b)+min(((n%m)*a),b)))<<endl;
+   if(n%m==0)
+    {
+        int tempX = (n/m)*b;
+        cout<<min(tempX,(n*a))<<endl;
+    }
+    else
+    {
+       // int tempA = (((n/m)*b)+((n%m)*a));
+       // cout<<tempA<<endl;
+      //  int tempB = (((n/m)*b)+b) ;
+      //  cout<<tempB<<endl;
+       cout<<min( n*a , min( (((n/m)*b)+((n%m)*a)) , (((n/m)*b)+b) ))<<endl;
+    }
     return 0;
-
 }
