786cfbce06ed2b7b6eb5400f3b30694ce54b1a7a
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-08-25 00:42:57 +0600

    Update 584A-Olesya and Rodion.cpp

--- codeforces/584A-Olesya and Rodion.cpp
+++ codeforces/584A-Olesya and Rodion.cpp
@@ -2,27 +2,25 @@
 using namespace std ;
 int main()
 {
-      ios_base::sync_with_stdio(0);
-       int  n;
-       int t;
-      cin>>n>>t;
-      if(n==1 and t ==10  )
-      {
-          cout<<"-1"<<endl;
-          return 0;
-      }
-      if(t==10)
-      {
-          for(int i=0;i<n-1;i++)
-      {
-          cout<<1;
-      }
-      cout<<0;
-          return  0;
-      }
-      for(int i=0;i<n;i++)
-      {
-          cout<<t;
-      }
-      return  0 ;
+    int n ,t;
+    cin>>n>>t;
+    if(n==1 and t==10)
+    {
+        cout<<"-1";
+    }
+    else
+    {
+        if(t==10)
+        {
+           cout<<1;
+           for(int i=1;i<n;i++)cout<<"0";
+        }
+        else{
+        for(int i=0;i<n;i++)
+        {
+            cout<<t;
+        }
+        }
+    }
+    return 0 ;
 }
