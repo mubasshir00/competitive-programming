c7c03e27c918b32947c76f339a172fd41a255821
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-08-25 00:17:03 +0600

    Create 1369A - FashionabLee.cpp

---
+++ codeforces/1369A - FashionabLee.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    long long n ;
+    cin>>n;
+    while(n--)
+    {
+        long long x ;
+        cin>>x;
+        if((x%4)==0)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0 ;
+}
