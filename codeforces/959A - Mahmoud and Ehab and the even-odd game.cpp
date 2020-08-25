ee540a6bc749a3a1181c327195f4b7bdcdad776f
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-08-26 03:11:23 +0600

    Create 959A - Mahmoud and Ehab and the even-odd game.cpp

---
+++ codeforces/959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    long long n,m;
+    cin>>n;
+    if((n&1)==0)cout<<"Mahmoud"<<endl;
+    else cout<<"Ehab"<<endl;
+    return 0;
+}
