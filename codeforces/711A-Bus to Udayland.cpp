e34c0bf428fc29a1e73b8864e1dd3946fce0a1d4
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-08-26 04:58:21 +0600

    Update 711A-Bus to Udayland.cpp

--- codeforces/711A-Bus to Udayland.cpp
+++ codeforces/711A-Bus to Udayland.cpp
@@ -3,51 +3,67 @@ using namespace std ;
 int main()
 {
     int n ;
-    bool f =false;
-    char a[1005][1005];
+    int f=0;
+    int count =0;
     cin>>n;
-    int m ;
-    m=n;
-
-    for(int i=0;i<n;i++)
+    char C[10000];
+    for(int i=1;i<=(n*5);i++)
     {
-        for(int j=0;j<5;j++)
-        {
-            cin>>a[i][j];
-        }
+        cin>>C[i];
     }
-    for(int i=0;i<n;i++)
+    if(n==1)
     {
-        for(int j=0;j<4;j++)
+        for(int i=1;i<=5;i++)
         {
-            if(a[i][j]=='O' && a[i][j+1]=='O')
+            if(C[i]=='O' and C[i+1]=='O')
             {
-                a[i][j]='+';
-                a[i][j+1]='+';
-                f=true;
+                f=1;
+                C[i]='+';
+                C[i+1]='+';
                 break;
             }
-
+            else f=0;
         }
-        if(f==true)break;
+
+        if(f==1)
+        {
+            cout<<"YES"<<endl;
+            for(int i=1;i<=5;i++)
+            {
+                cout<<C[i];
+            }
+        }
+        else cout<<"NO"<<endl;
     }
-    if(f==true)
+
+    else {
+    for(int i=1;i<=(n*5);i++)
+    {
+        if(C[i]=='O' and C[i+1]=='O')
+        {
+            if(i%5==0 and (i+1)%5==1)
+            {
+                count++;
+            }
+            else {
+            C[i]='+';
+            C[i+1]='+';
+            f=1;
+            break;
+            }
+        }
+        else f=0;
+    }
+    if(f==1)
     {
         cout<<"YES"<<endl;
-        for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<5;j++)
+        for(int i=1;i<=(n*5);i++)
         {
-            cout<<a[i][j];
+            cout<<C[i];
+            if(i%5==0)cout<<endl;
         }
-        cout<<endl;
     }
-    return 0 ;
+    else cout<<"NO"<<endl;
     }
-    else
-    {
-        cout<<"NO"<<endl;
-        return 0 ;
-    }
-    return 0 ;
+    return 0;
 }
