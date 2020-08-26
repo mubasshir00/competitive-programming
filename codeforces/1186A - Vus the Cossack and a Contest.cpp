a6c6fd9df71bc0b906ba5e6777515eb179c70c85
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-08-27 04:10:54 +0600

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ codeforces/1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+    if(n>m || n>k)
+    {
+        cout<<"NO"<<endl;
+    }
+    else  cout<<"YES"<<endl;
+    return 0;
+}
