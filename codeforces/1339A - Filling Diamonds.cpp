6c11c4bdaf515fcd2d38d53722964b60ddc61995
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-08-28 05:00:01 +0600

    Create 1339A - Filling Diamonds.cpp

---
+++ codeforces/1339A - Filling Diamonds.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    long long tc;
+    cin>>tc;
+    while(tc--)
+    {
+       long long n ;
+       cin>>n;
+       cout<<n<<endl;
+    }
+    return 0;
+}
