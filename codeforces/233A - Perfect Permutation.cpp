2c4e7d52f63648aeb153639456d0381f6e2ce313
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-09-11 03:03:05 +0600

    Update 233A - Perfect Permutation.cpp

--- codeforces/233A - Perfect Permutation.cpp
+++ codeforces/233A - Perfect Permutation.cpp
@@ -1,25 +1,18 @@
-    #include<bits/stdc++.h>
-    using namespace std ;
-     
-    void permute(int  s)
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if((n&1)==1)
+        cout<<"-1"<<endl;
+    else
     {
-       if(s%2!=0)
-       {
-           cout<<"-1"<<endl;
-       }
-       else{
-       for(int i=1;i<=s/2;i++)
-       {
-           cout<<(2*i)<<" "<<(2*i)-1<<" ";
-       }
-       }
-     
+        for(int i=2;i<=n;i=i+2)
+        {
+            cout<<i<<" "<<i-1<<" ";
+        }
     }
-     
-    int main()
-    {
-        int n;
-        cin>>n;
-        permute(n);
-        return 0 ;
-    }
+    return 0;
+}
