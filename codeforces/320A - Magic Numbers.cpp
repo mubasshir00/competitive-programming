2b7e8b2545129eb34064eaef2e8f46cd65ca97e0
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-09-25 01:56:17 +0600

    Update 320A - Magic Numbers.cpp

--- codeforces/320A - Magic Numbers.cpp
+++ codeforces/320A - Magic Numbers.cpp
@@ -1,31 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std ;
-typedef long long ll ;
-
 int main()
 {
-    string s ;
+    string s;
     cin>>s;
+    int count =0;
     int f =0;
-    for(ll i=0;i<s.length();i++)
-    {
-        if(s[i]== '1' or s[i]=='4' and s[0]=='1'  )
-        {
-
-           if(s.find("444")!=-1){
-            f=1;
-           }
-           else
-            f=0;
-
-        }
-        else
-        {
-            f=1;
-            break;
-        }
+for(int i=0;i<s.length();i++){
+     if (s[i]=='1' and s[i+1]=='4' and s[i+2] =='4'){
+        i=i+2;
+        f=1;
     }
-    if(f==1)cout<<"NO"<<endl;
-    else cout<<"YES"<<endl;
-    return 0 ;
+    else  if (s[i]=='1' and s[i+1]=='4'){
+        i=i+1;
+        f=1;
+    }
+    else if( (s[i]=='1')){
+        f=1;
+    }
+    else {
+        f=0;
+        break;
+    }
+}
+if(f==1)cout<<"YES"<<endl;
+else cout<<"NO"<<endl;
+    return 0;
 }
