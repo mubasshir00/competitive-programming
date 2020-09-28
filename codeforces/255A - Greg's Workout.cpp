c478ad7c0f4c14f1ec776113acf60efd2d911fa1
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-09-29 00:45:31 +0600

    Update 255A - Greg's Workout.cpp

--- codeforces/255A - Greg's Workout.cpp
+++ codeforces/255A - Greg's Workout.cpp
@@ -2,41 +2,32 @@
 using namespace std ;
 int main()
 {
-    int a[1000];
+   // vector < pair<int,string>>vec ;
+    int arr[3];
     int n ;
     cin>>n;
-    for(int i=1;i<=n;i++)
-    {
-        cin>>a[i];
+    int countCh =0 ,countB =0 , countBi =0;
+    for(int i=0;i<n;i++){
+        int x ;
+        cin>>x;
+        if(i%3 ==0 ){
+           countCh=countCh+x;
+        }
+        else if(i%3==1){
+           countB=countB+x;
+        }
+        else {
+           countBi=countBi+x;
+        }
     }
-    int sum=0,sum1=0,sum2=0;
-    for(int i=1;i<=n;i++)
-    {
-        if(i%3==1)
-        {
-            sum=sum+a[i];
-        }
-        if(i%3==2)
-        {
-            sum1=sum1+a[i];
-        }
-        if(i%3==0)
-        {
-            sum2=sum2+a[i];
-        }
-
-    }
-    if(sum>sum1 && sum>sum2)
-    {
+    if(countCh>countB and countCh>countBi){
         cout<<"chest"<<endl;
     }
-else if(sum2>sum && sum2>sum1)
-{
-    cout<<"back"<<endl;
-}
-   else cout<<"biceps"<<endl;
-  /* cout<<sum<<endl;
-   cout<<sum1<<endl;
-   cout<<sum2<<endl;*/
-    return 0 ;
+    else if(countB>countCh and countB>countBi){
+        cout<<"biceps"<<endl;
+    }
+    else if(countBi>countCh and countBi>countB) {
+        cout<<"back"<<endl;
+    }
+    return 0;
 }
