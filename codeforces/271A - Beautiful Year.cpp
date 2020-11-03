fe10987fa0b8d206d40708aa4f1035079571ab7e
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2020-11-03 11:54:02 +0600

    Update 271A - Beautiful Year.cpp

--- codeforces/271A - Beautiful Year.cpp
+++ codeforces/271A - Beautiful Year.cpp
@@ -1,45 +1,31 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std ;
 
-// Function to print unique digit numbers
-// in range from l to r.
-vector<int>v;
-void printUnique(int l, int r)
-{
-    // Start traversing the numbers
-    for (int i=l ; i<=r ; i++)
-    {
-        int num = i;
-        bool visited[10] = {false};
-
-        // Find digits and maintain its hash
-        while (num)
-        {
-            // if a digit occurs more than 1 time
-            // then break
-            if (visited[num % 10])
-                break;
-
-            visited[num%10] = true;
-
-            num = num/10;
-        }
-
-        // num will be 0 only when above loop
-        // doesn't get break that means the
-        // number is unique so print it.
-        if (num == 0)
-            v.push_back(i);
-    }
+bool check(int s){
+	set<int>S;
+	while(s!=0){
+		S.insert(s%10);
+		s=s/10;
+	}
+	if(S.size()==4){
+		return true;
+	}
+	else return false ;
 }
 
-// Driver code
 int main()
 {
-    int n ;
+    int n;
     cin>>n;
-    printUnique(n+1, 90000);
-    cout<<v[0];
-
-    return 0;
+    n++;
+    while(1){
+    if(!check(n)){
+    	n++;
+	}
+	else {
+		cout<<n<<endl;
+		break;
+	}
+}
+	return 0;
 }
