9c3da149df617737139a9064ad6571b155c54117
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2021-03-21 12:49:41 +0600

    Update 110A-Nearly Lucky Number.cpp

--- codeforces/110A-Nearly Lucky Number.cpp
+++ codeforces/110A-Nearly Lucky Number.cpp
@@ -2,26 +2,19 @@
 using namespace std ;
 int main()
 {
-    string a;
-    cin>>a;
-    int count =0,counta=0;
-    long long int len = a.length();
-    for(long long int i=0;i<len;i++)
+    string s ;
+    int count=0;
+    cin>>s;
+    for(int i=0;i<s.length();i++)
     {
-        if(a[i]=='4')
-        {
+        if(s[i]=='7' || s[i]=='4'){
             count++;
         }
-        if(a[i]=='7')
-        {
-            counta++;
-        }
     }
-    if((count+counta)==7 || (count+counta)==4)
+    if(count==7 || count==4)
     {
         cout<<"YES"<<endl;
     }
     else cout<<"NO"<<endl;
-
-    return 0 ;
+    return 0;
 }
