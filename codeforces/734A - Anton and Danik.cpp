9404facdd6fdca789a576037e757b23a5bdfa334
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2021-03-21 13:30:59 +0600

    Update 734A - Anton and Danik.cpp

--- codeforces/734A - Anton and Danik.cpp
+++ codeforces/734A - Anton and Danik.cpp
@@ -8,12 +8,10 @@ int main()
     cin>>s;
     int Da =0 ;
     int An =0 ;
-
     for(int i=0;i<n;i++)
     {
         if(s[i]=='D')Da++;
         if(s[i]=='A')An++;
-
     }
     if(An>Da)cout<<"Anton"<<endl;
     else if(Da>An)cout<<"Danik"<<endl;
