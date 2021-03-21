d87a4c3f6567dd81757abfb14edc6de263eb572d
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2021-03-21 12:09:08 +0600

    Update 122A - Lucky Division.cpp

--- codeforces/122A - Lucky Division.cpp
+++ codeforces/122A - Lucky Division.cpp
@@ -1,39 +1,27 @@
-    #include<bits/stdc++.h>
-    using namespace std ;
-     
-    int string_to_int_converter(string str)
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{
+    string s ;
+    cin>>s;
+    int count =0;
+    for(int i=0;i<s.length();i++)
     {
-        stringstream convert_to_int(str);
-        int x =0;
-        convert_to_int>>x;
-        return x ;
+        if(s[i]=='4' || s[i]=='7'){
+            count++;
+        }
     }
-     
-     
-    int main()
-    {
-       string s ;
-       cin>>s;
-       int new_int = string_to_int_converter(s);
-       int count=0;
-       for(int i=0;i<s.length();i++)
-       {
-           if(s[i]=='4' || s[i]=='7')
-           {
-               count++;
-           }
-       }
-       if(count==s.length())
-       {
-           cout<<"YES"<<endl;
-       }
-       else
-       {
-           if(new_int%4 ==0 || new_int%7==0 || new_int%47 ==0 || new_int%744==0)
-           {
-               cout<<"YES"<<endl;
-           }
-           else cout<<"NO"<<endl;
-       }
-        return 0 ;
+    if(count == s.length()){
+        cout<<"YES"<<endl;
     }
+    else {
+        stringstream strtoint(s);
+        int x ;
+        strtoint>>x;
+        if(x%7==0 || x%4==0 || x%47==0 || x%744==0 || x%74==0){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
