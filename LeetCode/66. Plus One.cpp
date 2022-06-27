472c858530510cb542375d1fa846056e6fb8bc25
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-06-27 07:20:17 +0600

    Create 66. Plus One.cpp

---
+++ LeetCode/66. Plus One.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+       for(int i=digits.size()-1;i>=0;i--){
+        if(digits[i]!=9){
+            digits[i] = digits[i]+1;
+            break;
+        } else  {
+          digits[i] = 0;
+        }
+    }
+
+    if(digits[0]==0){
+       digits.push_back(1);
+       reverse(digits.begin(),digits.end());
+    }
+   return digits;
+    }
+};
