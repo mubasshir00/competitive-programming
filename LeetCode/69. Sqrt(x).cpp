ab35cab2896a1e2ef1c25ec03f663aad5d8ebd59
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-06-28 08:51:07 +0600

    Update 69. Sqrt(x).cpp

--- LeetCode/69. Sqrt(x).cpp
+++ LeetCode/69. Sqrt(x).cpp
@@ -5,3 +5,28 @@ public:
        return floor(sqrt(x));
     }
 };
+
+//binary search
+class Solution {
+public:
+    int mySqrt(int x) {
+       int start = 1;
+    int end = x;
+    int res ;
+    if(x<2){
+        return x ;
+    }
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        if(mid == x/mid){
+            return mid ;
+        } else if(mid<x/mid){
+            start = mid + 1;
+            res = mid ;
+        } else {
+            end = mid -1;
+        }
+    }
+    return res ;
+    }
+};
