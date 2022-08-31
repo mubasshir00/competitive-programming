e7d57ecebecd5d7e1b221b3e729d9974e18f707b
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-08-31 06:58:07 +0600

    Update and rename convert_line_to_des.cpp to convert_word_line_to_des.cpp

--- algorithm/Cryptography/Des_data_encryption_standard/convert_word_line_to_des.cpp
+++ algorithm/Cryptography/Des_data_encryption_standard/convert_word_line_to_des.cpp
@@ -291,52 +291,64 @@ string toBinary(std::string const &str)
     return final_binary;
 }
 
-int main()
-{
-    
-    
-    // A 64 bit key
-    string plain_text ;
-    cin>>plain_text;
+string convert_to_hes (string str2){
+    //A 64 bit text
+    string plain_text = str2;
     string temp_plain_text = "";
     temp_plain_text = toBinary(plain_text);
     string key = "1010101010111011000010010001100000100111001101101100110011011101";
-    // A block of plain text of 64 bits
+    //A block of plain text 64 bits
     pt = temp_plain_text;
     string apt = pt;
-    // Calling the function to generate 16 keys
+    //calling the function to generate 16 keys
     generate_keys(key);
-    cout << "Plain text: " << pt << endl;
-    // Applying the algo
+    cout<<"Plain Text Word Binary : "<<pt<<endl;
+    //apply the algo
     string ct = DES();
-    cout << "Ciphertext: " << ct << endl;
+    cout<<"CipherText "<<ct<<endl;
     // Reversing the round_keys array for decryption
     int i = 15;
-    int j = 0;
-    while (i > j)
-    {
+    int j =0;
+    while(i>j){
         string temp = round_keys[i];
         round_keys[i] = round_keys[j];
         round_keys[j] = temp;
         i--;
         j++;
     }
-    pt = ct;
-    string decrypted = DES();
-    cout << "Decrypted text:" << endl;
-    int count_i =0;
-    cout<<decrypted.size()<<endl;
-    for (int i = 0; i < decrypted.size(); i++)
-    {
-        if((i+1)%8==0){
-            cout << decrypted[i] << " ";
-        } else {
-            cout<<decrypted[i];
+    pt = ct ;
+    string decrypted_text = DES();
+    return decrypted_text;
+}
+
+int main()
+{
+    string line_text = "";
+    getline(cin,line_text);
+    istringstream iss(line_text);
+    vector<string>V;
+    while(iss){
+        string str2;
+        iss >> str2;
+        // cout<<str2<<endl;
+        string temp_str2 = "";
+
+        string temp_str = convert_to_hes(str2);
+
+        for (int i = 0; i < temp_str.size();i++){
+            if((i+1)%8 ==0){
+               temp_str2.push_back(temp_str[i]);
+               temp_str2.push_back(' ');
+            } else {
+                temp_str2.push_back(temp_str[i]);
+            }
         }
+
+        V.push_back(temp_str2);
     }
-    // Comapring the initial plain text with the decrypted text
-    if (decrypted == apt)
-    {
-        cout << "Plain text encrypted and decrypted successfully." << endl;
+    for(int i=0;i<V.size()-1;i++){
+        cout<<"word "<<i<<" "<<V[i]<<endl;
     }
+
+   
 }
