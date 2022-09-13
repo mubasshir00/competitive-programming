671dfbb87b3504cb3fe08b3305bcdc200382aac3
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-09-13 08:16:24 +0600

    Update insertion.cpp

--- algorithm/Linked_List/insertion.cpp
+++ algorithm/Linked_List/insertion.cpp
@@ -1,83 +1,90 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
+struct Node
 {
-public:
     int data;
-    Node *next;
-};
+    struct Node *next;
+} *first = NULL; // first is glbal
 
-//insert in to end
-void append(Node **head_ref, int new_data)
+void create_linked_list(int arr[], int n)
 {
-    /* 1. allocate node */
-    Node *new_node = new Node();
-
-    Node *last = *head_ref; /* used in step 5*/
-
-    /* 2. put in the data */
-    new_node->data = new_data;
-
-    /* 3. This new node is going to be
-    the last node, so make next of
-    it as NULL*/
-    new_node->next = NULL;
-
-    /* 4. If the Linked List is empty,
-    then make the new node as head */
-    if (*head_ref == NULL)
+    struct Node *temp, *last;
+    int i;
+    // allocate unintialize memory of struct
+    first = (struct Node *)malloc(sizeof(struct Node *));
+    // store 1st array element
+    first->data = arr[0];
+    // next address is always null for 1st
+    first->next = NULL;
+    last = first;
+    for (int i = 1; i < n; i++)
     {
-        *head_ref = new_node;
+        temp = (struct Node *)malloc(sizeof(struct Node *));
+        temp->data = arr[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+int count(struct Node *p)
+{
+    int count = 0;
+    int sum = 0;
+    while (p != NULL)
+    {
+        sum = sum + p->data;
+        p = p->next;
+        count++;
+    }
+    return count;
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        // cout<<first<<" ";
+        cout << p->data << " ";
+        // cout<<p->next<<" ";
+        p = p->next;
+    }
+}
+
+void insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+    if (index < 0 || index > count(p))
+    {
         return;
     }
-
-    /* 5. Else traverse till the last node */
-    while (last->next != NULL)
+    t = (struct Node *)malloc(sizeof(struct Node));
+    t->data = x;
+    if (index == 0)
     {
-        last = last->next;
+        t->next = first;
+        first = t;
     }
-
-    /* 6. Change the next of last node */
-    last->next = new_node;
-    return;
-}
-// insert data in front
-void push(Node **head_ref, int new_data)
-{
-    // allocate node
-    Node *new_node = new Node();
-
-    // put in the data
-    new_node->data = new_data;
-
-    new_node->next = (*head_ref);
-
-    (*head_ref) = new_node;
-}
-
-void printList(Node *node)
-{
-    while (node != NULL)
+    else
     {
-        cout << " Node Address " << node->next << " Node Data " << node->data;
-        node = node->next;
-        cout << endl;
+        for (i = 0; i < index - 1; i++)
+            p = p->next;
+        t->next = p->next;
+        p->next = t;
     }
 }
 
 int main()
 {
-    int n;
-    Node *head = NULL;
-    cin >> n;
-    while (n--)
-    {
-        /* code */
-        int x;
-        cin >> x;
-        push(&head, x);
-    }
-    printList(head);
+    int arr[] = {4, 5, 1, 23, 1, 50, 70, 10};
+    create_linked_list(arr, 8);
+    int total_linked_list = count(first);
+
+    insert(first, 0, 100);
+
+    display(first);
+
     return 0;
 }
