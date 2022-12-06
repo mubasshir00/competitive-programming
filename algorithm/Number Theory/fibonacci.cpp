f8c9361c748b0278b916477d2670ef13a17631f5
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-12-06 23:37:33 +0600

    Create fibonacci.cpp

---
+++ algorithm/Number Theory/fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fibon(int n)
+{
+    int fib[n];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
+}
+int main()
+{
+    long long int n;
+    cin >> n;
+    cout << fibon(n) << endl;
+    return 0;
+}
